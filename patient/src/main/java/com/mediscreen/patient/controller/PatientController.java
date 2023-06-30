3d40f3d3a867cd47822a16b4d8af822fe0db6c32
Anatoly <anatolyrytik@gmai.com>
2023-06-30 16:34:27 +0200

    add PatientController

---
+++ patient/src/main/java/com/mediscreen/patient/controller/PatientController.java
@@ -0,0 +1,65 @@
+package com.mediscreen.patient.controller;
+
+import com.mediscreen.patient.dto.PatientDto;
+import com.mediscreen.patient.service.PatientService;
+import com.mediscreen.patient.util.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientDto> createPatient(@Valid @RequestBody PatientDto patientDto) {
+        PatientDto createdPatient = patientService.createPatient(patientDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPatient);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPatient(@PathVariable @Min(1) Long id) {
+        try {
+            PatientDto patientDto = patientService.getPatient(id);
+            return ResponseEntity.ok(patientDto);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientDto>> getAllPatients() {
+        List<PatientDto> patients = patientService.getAllPatients();
+        return ResponseEntity.ok(patients);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updatePatient(@PathVariable @Min(1) Long id, @Valid @RequestBody PatientDto patientDto) {
+        try {
+            PatientDto updatedPatient = patientService.updatePatient(id, patientDto);
+            return ResponseEntity.ok(updatedPatient);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable @Min(1) Long id) {
+        try {
+            patientService.deletePatient(id);
+            return ResponseEntity.noContent().build();
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
+
