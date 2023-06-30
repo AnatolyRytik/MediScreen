31c929ee1e81e862a6f47c4fdb7cd9994c503bca
Anatoly <anatolyrytik@gmai.com>
2023-06-30 12:06:14 +0200

    create model class

---
+++ patient/src/main/java/com/mediscreen/patient/model/Patient.java
@@ -0,0 +1,34 @@
+package com.mediscreen.patient.model;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}
+
